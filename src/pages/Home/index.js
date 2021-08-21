f203f897093772f1e695a78f3cfba321c916d8a0
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-21 22:09:40 +0700

    slicing slider with library react-native-image-slider-box

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from '../../components';
+import { BannerSlider, Header } from '../../components';
 import { colors } from '../../utils';
 
 const Home = () => {
     return (
         <View style={styles.page}>
             <Header />
+            <BannerSlider />
             <Text>Home Page</Text>
         </View>
     )
